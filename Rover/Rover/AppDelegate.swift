471a0a7e501d484e6421b8ca4e53d5dbe634f393
Sydney Wang <sydney.zy.wang@gmail.com>
2019-03-13 22:06:57 -0400

    added playtime; UI improvements (#3)

--- Rover/Rover/AppDelegate.swift
+++ Rover/Rover/AppDelegate.swift
@@ -40,7 +40,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        let defaults = UserDefaults.standard
+        let defaultValue = ["totalPlayTime" : 0]
+        defaults.register(defaults: defaultValue)
+    }
 
 }
 
