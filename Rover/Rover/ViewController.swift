028d5f7de52456038584c07bdacdb6065d728eed
Sydney Wang <sydney.zy.wang@gmail.com>
2019-03-13 21:55:36 -0400

    added playtime; UI improvements

--- Rover/Rover/ViewController.swift
+++ Rover/Rover/ViewController.swift
@@ -21,43 +21,99 @@ class ViewController: UIViewController, UITextViewDelegate {
         case unknown
     }
 
-    @IBOutlet weak var thatlabel: UILabel!
-    var dataCharacteristic : Characteristic?
+    @IBOutlet weak var statusLabel: UILabel!
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
+
     @IBOutlet weak var mybutton: UIButton!
     
-    
     @IBOutlet weak var readButton: UIButton!
+    
+    @IBOutlet weak var sessionPlayTimeLabel: UILabel!
+    
+    @IBOutlet weak var totalPlayTimeLabel: UILabel!
 
+    @IBOutlet weak var spinwheel: UIActivityIndicatorView!
+
+    @IBOutlet weak var dogPicture: UIImageView!
+
+    @IBOutlet weak var coolDogePicture: UIImageView!
+    
     @IBAction func onModeChange(_ sender: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-            thatlabel.text = "avoid"
             write(0)
         case 1:
-            thatlabel.text = "chase"
             write(1)
+        case 2:
+            write(2)
         default:
             break
         }
+    }
+
+    var dataCharacteristic : Characteristic?
+
+    // update play time every minute
+    var sessionPlayTime = 0;
+    var totalPlayTime = 0;
+    var timer: Timer!
+
+    @objc func updatePlayTime() {
+        sessionPlayTime += 1
+        totalPlayTime += 1
+        self.updatePlayTimeLabels()
+    }
+
+    func updatePlayTimeLabels() {
+        self.totalPlayTimeLabel.text = String(totalPlayTime) + " min"
+        self.sessionPlayTimeLabel.text = String(sessionPlayTime) + " min"
+    }
+
+    func readTotalPlayTime() {
+        let defaults = UserDefaults.standard
+        totalPlayTime = defaults.integer(forKey: "totalPlayTime")
+    }
+
+    func writeTotalPlayTime() {
+        let defaults = UserDefaults.standard
+        defaults.set(totalPlayTime, forKey: "totalPlayTime")
+    }
+
+    func onDeviceDisconnection() {
+        self.statusLabel.text = "Searching for Rover..."
+        self.spinwheel.startAnimating()
+        self.dogPicture.isHidden = true
+        // stop counting play time
+        self.timer = nil
+    }
+    
+    func onDeviceConnection() {
+        self.statusLabel.text = "Connected to Rover."
+        self.spinwheel.stopAnimating()
+        self.dogPicture.isHidden = false
+        // start counting play time
+        self.timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(self.updatePlayTime), userInfo: nil, repeats: true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
+        self.onDeviceDisconnection()
+
+        self.readTotalPlayTime()
+        self.updatePlayTimeLabels()
+
         let serviceUUID = CBUUID(string: "FFE0")
         let characteristicUUID = CBUUID(string: "FFE1")
         var peripheral: Peripheral?
 
-        //initialize a central manager with a restore key. The restore key allows to resuse the same central manager in future calls
+        //the restore key allows to resuse the same central manager in future calls
         let manager = CentralManager(options: [CBCentralManagerOptionRestoreIdentifierKey : "CentralMangerKey" as NSString])
 
-        //A future stram that notifies us when the state of the central manager changes
         let stateChangeFuture = manager.whenStateChanges()
 
-        //handle state changes and return a scan future if the bluetooth is powered on.
+        //handle state changes and return a scan future if the bluetooth is powered on
         let scanFuture = stateChangeFuture.flatMap { state -> FutureStream<Peripheral> in
             switch state {
             case .poweredOn:
@@ -86,7 +142,6 @@ class ViewController: UIViewController, UITextViewDelegate {
             guard let appError = error as? AppError else {
                 return
             }
-            print("scan failure")
             switch appError {
             case .invalidState:
                 break
@@ -116,8 +171,6 @@ class ViewController: UIViewController, UITextViewDelegate {
             return peripheral.connect(connectionTimeout: 10, capacity: 5)
         }
     
-        print("finished connection")
-    
         //discover service in order be able to access its characteristics
         let discoveryFuture = connectionFuture.flatMap { _ -> Future<Void> in
             guard let peripheral = peripheral else {
@@ -133,14 +186,11 @@ class ViewController: UIViewController, UITextViewDelegate {
                 }
                 peripheral = discoveredPeripheral
                 DispatchQueue.main.async {
-                    self.thatlabel.text = "Discovered service \(service.uuid.uuidString). Trying to discover characteristics"
-                    print(self.thatlabel.text!)
+                    print("Discovered service \(service.uuid.uuidString). Trying to discover characteristics")
                 }
-                //we have discovered the service, the next step is to discover the "ec0e" characteristic
+                //we have discovered Rover's BT module, the next step is to discover its characteristic
                 return service.discoverCharacteristics([characteristicUUID])
         }
-    
-        print("finished discovery")
     
         /**
          1- checks if the characteristic is correctly discovered
@@ -155,11 +205,10 @@ class ViewController: UIViewController, UITextViewDelegate {
             }
             self.dataCharacteristic = dataCharacteristic
             DispatchQueue.main.async {
-                self.thatlabel.text = "Discovered characteristic \(dataCharacteristic.uuid.uuidString)."
+                self.onDeviceConnection()
+                print("Discovered characteristic \(dataCharacteristic.uuid.uuidString).")
             }
-            //            DispatchQueue.main.async {
-            // TODO: add some UI that indicates connection
-            //            }
+
             //read the data from the characteristic
             self.read()
             //Ask the characteristic to start notifying for value change
@@ -179,8 +228,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         dataFuture.onSuccess { data in
             let s = String(data:data!, encoding: .utf8)
             DispatchQueue.main.async {
-                self.thatlabel.text = "notified value is \(String(describing: s))"
-                print(String(describing: s))
+                print("notified value is \(String(describing: s))")
             }
         }
     
@@ -188,6 +236,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         dataFuture.onFailure { error in
             switch error {
             case PeripheralError.disconnected:
+                print("attempt to reconnecct")
                 peripheral?.reconnect()
             case AppError.serviceNotFound:
                 break
@@ -215,26 +264,22 @@ class ViewController: UIViewController, UITextViewDelegate {
             //the value is in the dataValue property
             let s = String(data:(self.dataCharacteristic?.dataValue)!, encoding: .utf8)
             DispatchQueue.main.async {
-                self.thatlabel.text = "Read value is \(String(describing: s))"
-                print(self.thatlabel.text!)
+                print("Read value is \(String(describing: s))")
             }
         }
         readFuture?.onFailure { (_) in
-            self.thatlabel.text = "read error"
+            DispatchQueue.main.async {
+                print("read error")
+            }
         }
     }
     
     func write(_ mode: integer_t){
-        var text:String;
-        if (mode == 0) {
-            text = "mode_avoid\r\n"
-        } else {
-            text = "mode_chase\r\n";
-        }
+        let text = String(mode)
         let writeType: CBCharacteristicWriteType = .withoutResponse
         let writeFuture = self.dataCharacteristic?.write(data: text.data(using: .utf8)!, type: writeType)
         writeFuture?.onSuccess(completion: { (_) in
-            print("wrote value " + text + "successfully")
+            print("wrote value " + text + " successfully")
         })
         writeFuture?.onFailure(completion: { (e) in
             print("write failed")
